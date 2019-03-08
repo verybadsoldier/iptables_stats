79acecb5926435749f0266acd3dbe7463efc681b
verybadsoldier <vbs@springrts.de>
2019-03-08 13:21:19 +0100

    improved handling of none existing chains

--- iptables_stats/reading_modules/iptables.py
+++ iptables_stats/reading_modules/iptables.py
@@ -1,6 +1,14 @@
 import iptc
+import logging
 import subprocess
 import re
+
+
+_logger = logging.getLogger(__name__)
+
+
+class RuleNotFoundException(Exception):
+    pass
 
 
 def _find_rule_idx(chain, regex):
@@ -16,27 +24,42 @@ def _find_rule_idx(chain, regex):
         if not regex.match(rule):
             continue
         if rule_no is not None:
-            raise Exception(f"Regular expression '{regex}' found multiple matching rules in chain '{chain}'!")
+            raise RuleNotFoundException(f"Regular expression '{regex}' found multiple matching rules in chain '{chain}'!")
             return i
         rule_no = i
 
     if rule_no is None:
-        raise Exception(f"Could not find rule in chain '{chain}' that matches regular expression: '{regex}'")
+        raise RuleNotFoundException(f"Could not find rule in chain '{chain}' that matches regular expression: '{regex}'")
     return rule_no
 
 
-def get_rule_counters(chain, regex):
-    rule_no = _find_rule_idx(chain, regex)
+def get_rule_counters(chain_name, regex):
+    try:
+        rule_no = _find_rule_idx(chain_name, regex)
 
-    table = iptc.Table(iptc.Table.FILTER)
-    chain = iptc.Chain(table, chain)
-    rules = list(chain.rules)
-    counters = rules[rule_no].get_counters()
+        table = iptc.Table(iptc.Table.FILTER)
+        if table.is_chain(chain_name):
+            chain = iptc.Chain(table, chain_name)
+            rules = list(chain.rules)
+            counters = rules[rule_no].get_counters()
+        else:
+            _logger.debug(f"Chain '{chain_name}' does not exist")
+            counters = (0, 0)  # return 0 for non-existing chains
+    except RuleNotFoundException:
+        _logger.debug(f"Chain '{chain_name}' does not exist (reported by exception)")
+        counters = (0, 0)
 
     return dict(packet_count=counters[0], byte_count=counters[1])
 
 
-def get_rule_count(chain, offset=0):
+def get_rule_count(chain_name, offset=0):
+    _logger.debug(f"Querying rule count in chain '{chain_name}' using offset {offset}")
     table = iptc.Table(iptc.Table.FILTER)
-    chain = iptc.Chain(table, chain)
-    return dict(rule_count=len(chain.rules) + offset)
+    if table.is_chain(chain_name):
+        chain = iptc.Chain(table, chain_name)
+        rule_count = len(chain.rules) + offset
+    else:
+        _logger.debug(f"Chain '{chain_name}' does not exist")
+        rule_count = 0  # we return 0 for non-existing chains
+    _logger.debug(f"Rule count in '{chain_name}' is {rule_count} (offset applied)")
+    return dict(rule_count=rule_count)
